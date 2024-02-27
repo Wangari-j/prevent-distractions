0a33f9e27bf0f98042d52c091f74349bc41d0172
Wangari-j <wangarijudy@gmail.com>
2024-02-27 23:48:06 +0300

    functions

---
+++ python.py
@@ -0,0 +1,15 @@
+def prevent_distraction(str):
+    if "anime" == str:
+        return "No!"
+    elif "vine" == str:
+        return "No!"
+    elif "meme" == str:
+        return "No!"
+    elif "roasts" == str:
+        return "No!"
+    elif "Danny Devito" == str:
+        return "No!"
+    else:
+        return "Safe watching"
+    
+    print(prevent_distraction(anime))
